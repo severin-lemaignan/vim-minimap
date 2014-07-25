severin-lemaignan/vim-minimap

commit c0d1130e31d747116706e4dcda0d275bf856a15a

using Canvas.rows() instead of Canvas.frame()

autoload/minimap.vim
@@ -279,10 +279,8 @@ def draw(lengths, startline = 0):
         for x in range(2 * min(int(l * HORIZ_SCALE), WIDTH)):
             c.set(x, y)
 
-    lines = c.frame().split('\n')
-
     # pad with spaces to ensure uniform block highligthing
-    return [unicode(line, "utf-8").ljust(WIDTH) for line in lines]
+    return [unicode(line, "utf-8").ljust(WIDTH) for line in c.rows()]
 
 lengths = []
 

commit 5aa5e31812f3e5588fb5d95e73002f5fe1346cb5

No need to decode utf8 when using Canvas.row
Fix decoding error introduced by c0d1130e31d747116706e4dcda0d275bf856a15a

autoload/minimap.vim
@@ -280,7 +280,7 @@ def draw(lengths, startline = 0):
             c.set(x, y)
 
     # pad with spaces to ensure uniform block highligthing
-    return [unicode(line, "utf-8").ljust(WIDTH) for line in c.rows()]
+    return [line.ljust(WIDTH) for line in c.rows()]
 
 lengths = []
 